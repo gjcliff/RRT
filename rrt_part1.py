ae10ace3d93ec2538d130199a4bb1f1094637a39
Graham Clifford <gjcliff@gmail.com>
2023-09-12 15:30:53 -0500

    Tree is working finally. I will now work on getting the plot to generate in real time

--- rrt_part1.py
+++ rrt_part1.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 
-import matplotlib as plot
+import matplotlib.pyplot as plt
 import numpy as np
 import imageio.v3 as im
-import time
 
 # hash map and index each node
 # linked lists?
@@ -18,51 +17,85 @@ import time
 
 G = {} # this is the graph that will hold all the nodes
 delta = 1 # this is the incremental distance
-D = np.array([100,100]) # this is the domain
-qInit = np.array([50,50])
+D = (100,100) # this is the domain
+qInit = (50,50)
+K = 1000
 
 def RANDOM_CONFIGURATION(D):
-    rng = np.random.Generator(seed=60)
+    rng = np.random.default_rng()
     
-    x = rng.integers(low=0,high=D[0]+1)
-    y = rng.integers(low=0,high=D[1]+1)
+    x = rng.random()*D[0]
+    y = rng.random()*D[1]
 
-    qRand = np.array([x,y])
+    qRand = (x,y) 
+    # print(f"qRand: {qRand}")
 
     return qRand
 
 def NEAREST_VERTEX(qRand, G): 
-    distance = 0
-    qNear = ()
+
+    distance = max(D)
+
+    qRand_temp = np.array(qRand)
+
+    qNear = tuple()
     for k in G:
-        distance_temp = np.linalg.norm(k - qRand)
-        if distance_temp > distance:
+        k_temp = np.array(k)
+        distance_temp = np.linalg.norm(k_temp - qRand_temp)
+        if distance_temp < distance:
             distance = distance_temp
             qNear = k
     return qNear
 
 def NEW_CONFIGURATION(qNear, qRand, delta):
 
-    slope = calc_slope(qNear, qRand)
+    # slope = calc_slope(qRand, qNear)
 
-    theta = np.tan(slope)
-    xDiff = np.cos(theta)
-    yDiff = np.sin(theta)
+    xdir = qRand[0] - qNear[0]
+    ydir = qRand[1] - qNear[1]
 
-    qNew = np.array([xDiff,yDiff])
+    theta = np.arctan2(ydir,xdir)
+    xDiff = np.cos(theta) * delta
+    yDiff = np.sin(theta) * delta
+    help = np.sin(theta)
+
+    qNew = (qNear[0] + xDiff, qNear[1] + yDiff)
 
     return qNew
 
-
-
 def rrt_algo(qInit, K, delta, D):
-    G[qInit] = []
+
+    plt.ion
+    fig, ax = plt.subplots()
+
+    plot = ax.scatter([],[])
+    ax.set_xlim(0,D[0])
+    ax.set_ylim(0,D[1])
+
+    G_temp = {qInit: []}
+    G.update(G_temp)
     for i in range(K):
         qRand = RANDOM_CONFIGURATION(D)
         qNear = NEAREST_VERTEX(qRand,G)
         qNew = NEW_CONFIGURATION(qNear,qRand,delta)
-        G[qNear] = G[qNear].append(qNew)
-        G[qNew] = []
+        # temp = G[qNear]
+        # G[qNear] = temp.append(qNew)
+        
+        # G[qNew] = list()
+
+        G_temp = {qNew: []}
+        G.update(G_temp)
+        # print(f"G[qNear]: {G[qNear]}")
+
+        # newPoint = np.array(qNew)
+
+        # array = plot.get_offsets()
+        # array = np.append(array,newPoint)
+        # plot.set_offsets(array)
+        
+        # fig.canvas.draw()
+
+    return G
 
 def calc_slope(point1, point2):
     x1, y1 = point1
@@ -73,7 +106,17 @@ def calc_slope(point1, point2):
     return slope
 
 def main():
-    print("hi")
 
-if "__name__" == "__main__":
+    G = rrt_algo(qInit, K, delta, D)
+
+    nodes = G.keys()
+    xValues, yValues = zip(*nodes)
+
+    plt.scatter(xValues,yValues,s=1)
+    # plt.plot(xValues,yValues)
+    plt.show()
+
+
+
+if __name__ == "__main__":
     main()
